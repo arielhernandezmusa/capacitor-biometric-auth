92c091ad1f33ef3785f88962551742a76498c894
lilong <lilong@gmail.com>
2020-05-02 15:49:40 +0900

    Update Plugin.swift
    faceid

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -14,7 +14,7 @@ public class BiometricAuth: CAPPlugin {
         let localAuthenticationContext = LAContext()
         localAuthenticationContext.localizedFallbackTitle = "Use Passcode"
         if localAuthenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
-            call.resolve(["has": true])
+            call.resolve(["has": true, "faceID": localAuthenticationContext.biometryType == .faceID, "touchID": localAuthenticationContext.biometryType == .touchID])
         } else {
             guard let error = authError else {
                 return
@@ -39,18 +39,18 @@ public class BiometricAuth: CAPPlugin {
                 switch error.code {
                 case LAError.touchIDLockout.rawValue:
                     errorCode = 2
-
+                    
                 case LAError.touchIDNotAvailable.rawValue:
                     errorCode = 1
-
+                    
                 case LAError.touchIDNotEnrolled.rawValue:
                     errorCode = 3
-
+                    
                 default:
                     errorCode = 999
                 }
             }
-        
+            
             call.resolve(["has": false, "status": errorCode])
         }
     }
@@ -58,45 +58,53 @@ public class BiometricAuth: CAPPlugin {
     @objc func verify(_ call: CAPPluginCall) {
         let localAuthenticationContext = LAContext()
         let reasonString = call.getString("reason") ?? "To access the secure data"
-        localAuthenticationContext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reasonString) { success, evaluateError in
+        
+        localAuthenticationContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString) { success, evaluateError in
             if success {
                 call.resolve(["verified": true])
             } else {
-                var errorCode = 0
-                guard let error = evaluateError else {
-                    return
-                }
-                switch error._code {
-                    
-                case LAError.authenticationFailed.rawValue:
-                    errorCode = 10 //"The user failed to provide valid credentials"
-                    
-                case LAError.appCancel.rawValue:
-                    errorCode = 11 // "Authentication was cancelled by application"
-                    
-                case LAError.invalidContext.rawValue:
-                    errorCode = 12 // "The context is invalid"
-                    
-                case LAError.notInteractive.rawValue:
-                    errorCode = 13 // "Not interactive"
-                    
-                case LAError.passcodeNotSet.rawValue:
-                    errorCode = 14 // "Passcode is not set on the device"
-                    
-                case LAError.systemCancel.rawValue:
-                    errorCode = 15 // "Authentication was cancelled by the system"
-                    
-                case LAError.userCancel.rawValue:
-                    errorCode = 16 // "The user did cancel"
-                    
-                case LAError.userFallback.rawValue:
-                    errorCode = 17 // "The user chose to use the fallback"
-                    
-                default:
-                    errorCode = self.evaluatePolicyFailErrorMessageForLA(errorCode: error._code)
-                }
                 
-                call.reject("Auth failed", nil, ["verified": false, "status": errorCode] as? Error)
+                localAuthenticationContext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reasonString) { success, evaluateError in
+                    if success {
+                        call.resolve(["verified": true])
+                    } else {
+                        var errorCode = 0
+                        guard let error = evaluateError else {
+                            return
+                        }
+                        switch error._code {
+                            
+                        case LAError.authenticationFailed.rawValue:
+                            errorCode = 10 //"The user failed to provide valid credentials"
+                            
+                        case LAError.appCancel.rawValue:
+                            errorCode = 11 // "Authentication was cancelled by application"
+                            
+                        case LAError.invalidContext.rawValue:
+                            errorCode = 12 // "The context is invalid"
+                            
+                        case LAError.notInteractive.rawValue:
+                            errorCode = 13 // "Not interactive"
+                            
+                        case LAError.passcodeNotSet.rawValue:
+                            errorCode = 14 // "Passcode is not set on the device"
+                            
+                        case LAError.systemCancel.rawValue:
+                            errorCode = 15 // "Authentication was cancelled by the system"
+                            
+                        case LAError.userCancel.rawValue:
+                            errorCode = 16 // "The user did cancel"
+                            
+                        case LAError.userFallback.rawValue:
+                            errorCode = 17 // "The user chose to use the fallback"
+                            
+                        default:
+                            errorCode = self.evaluatePolicyFailErrorMessageForLA(errorCode: error._code)
+                        }
+                        
+                        call.reject("Auth failed", nil, ["verified": false, "status": errorCode] as? Error)
+                    }
+                }
             }
         }
     }
